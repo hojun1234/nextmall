3206aa786b18a1c357c0baf1dd971d2fe670bbd9
kim ho jun <h918329@gmail.com>
2022-11-03 12:14:35 +0900

    "10주차 내용"

--- utils/data.js
+++ utils/data.js
@@ -1,4 +1,21 @@
+import bcrypt from "bcryptjs";
+
 const data = {
+  users: [
+    {
+      name: "john",
+      email: "admin@examle.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: true,
+    },
+    {
+      name: "Jane",
+      email: "user@example.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: false,
+    },
+  ],
+
   products: [
     {
       name: "Free Shirt",
