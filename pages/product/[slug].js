615655cea3886bf8672989003114e5bf6988819a
kim ho jun <h918329@gmail.com>
2022-11-17 12:26:22 +0900

    12주차

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -2,16 +2,19 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import Layout from "../../components/Layout";
-import data from "../../utils/data";
+//import data from "../../utils/data";
 import React, { useContext } from "react";
 import { Store } from "../../utils/Store";
+import db from "../../utils/db";
+import Product from "../../models/Product";
+import axios from 'axios'
 
-export default function ProductScreen() {
+export default function ProductScreen({ product }) {
   const router = useRouter();
   const { state, dispatch } = useContext(Store);
-  const { query } = useRouter();
-  const { slug } = query;
-  const product = data.products.find((x) => x.slug === slug);
+  ///// const { query } = useRouter();
+  ///const { slug } = query;
+  /// const product = data.products.find((x) => x.slug === slug);
 
   if (!product) {
     return <Layout title="Produt Not Found">Produt Not Found</Layout>;
@@ -20,11 +23,12 @@ export default function ProductScreen() {
   const addToCartHandler = async () => {
     const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
     const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
 
-    if (product.countInStock < quantity) {
-      alert("sorry,재고가 없습니다.");
-      return;
+    if (data.countInStock < quantity) {
+      return toast.error("Sorry. Product is out of stock");
     }
+
     dispatch({ type: "CART_ADD_ITEM", payload: { ...product, quantity } });
     router.push("/cart");
   };
@@ -79,4 +83,18 @@ export default function ProductScreen() {
       </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  await db.connect();
+  const product = await Product.findOne({ slug }).lean();
+  await db.disconnect();
+  return {
+    props: {
+      product: product ? db.convertDocToObj(product) : null,
+    },
+  };
 }
